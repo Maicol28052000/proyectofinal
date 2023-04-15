8a5d00ded686ad1814871f421ac653aae6ce0473
Maicol28052000 <alankelokhe@gmail.com>
2023-04-15 16:00:35 -0300

    casi listo banner

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PresentacionComponent
   ],
   imports: [
     BrowserModule,
