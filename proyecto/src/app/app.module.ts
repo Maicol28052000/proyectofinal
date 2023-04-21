f9639a4808c407774839e34ba04ed87922246791
Maicol28052000 <alankelokhe@gmail.com>
2023-04-21 00:12:41 -0300

    agregamos 2 componentes

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PresentacionComponent } from './components/presentacion/presentacion.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PresentacionComponent,
-    InicioComponent
+    InicioComponent,
+    AboutComponent,
+    ContactComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule,
